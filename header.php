a40d0c60a10045035bd713cb065b263a789ca6a8
lahlaliwassim2 <wlahlali34@gmail.com>
2022-01-21 13:31:08 +0100

    appliquer le principe de développement DRY avec PHP.

---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="bg-white row align-items-center py-2">
+                <div class="col-1">
+                  <i class="bi bi-arrow-left-circle"></i>
+                </div>
+                <div class="col-6"></div>
+                <div class="col-4">
+                  <div class="input-group">
+                    <input
+                      type="search"
+                      id="form1"
+                      class="form-control"
+                      placeholder="search"
+                    />
+                    <button type="button" class="btn bg-light">
+                      <i class="bi bi-search"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="col-1">
+                  <i class="bi bi-bell"></i>
+                </div>
+              </div>
