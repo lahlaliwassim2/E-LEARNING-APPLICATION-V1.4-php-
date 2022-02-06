c3f401cc6236978f3ce624cf6d6bd7ccdc1868ca
lahlaliwassim2 <wlahlali34@gmail.com>
2022-02-06 20:52:24 +0100

    ajout d un fichier php pour rendre les cartes de page dashbord dynamic

---
+++ outil.php
@@ -0,0 +1,12 @@
+<?php
+include 'conexion.php';
+$select="SELECT * FROM students";
+$query=$conn->query($select);
+$compteur=mysqli_num_rows($query);
+?>
+
+<?php
+ $somme= " SELECT SUM(amount_paid) as somme FROM payment_details";
+ if(! $result= $conn->query($somme)){ die("connection failed". $conn->error);}
+ else{$somme=$result->fetch_assoc();}
+?>
