d9dcbe4e0b0ba9127513bcae1ed35f29a237144e
lahlaliwassim2 <wlahlali34@gmail.com>
2022-02-07 21:49:55 +0100

    ajouter la commande php pour rendre la carte de cours sur la page dashbord dynamic

--- outil.php
+++ outil.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexion.php';
 $select="SELECT * FROM students";
-$query=$conn->query($select);
+$query=$conn->query($select); 
 $compteur=mysqli_num_rows($query);
 ?>
 
@@ -9,4 +9,12 @@ $compteur=mysqli_num_rows($query);
  $somme= " SELECT SUM(amount_paid) as somme FROM payment_details";
  if(! $result= $conn->query($somme)){ die("connection failed". $conn->error);}
  else{$somme=$result->fetch_assoc();}
+?>
+
+
+<?php 
+include 'conexion.php';
+$select="SELECT * FROM courses";
+$query=$conn->query($select);
+$compteurcourses=mysqli_num_rows($query);
 ?>
