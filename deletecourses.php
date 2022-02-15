b1dae0c1d3101fccc2001388a158bf088904d7c4
lahlaliwassim2 <wlahlali34@gmail.com>
2022-02-15 09:07:27 +0100

    modifier la liaison pour que l utulisateur reste dans la meme page pendant l action de suprimmer

--- deletecourses.php
+++ deletecourses.php
@@ -4,6 +4,11 @@ if(isset($_GET['id'])){
     $id=$_GET['id'];
 $delete="DELETE FROM courses WHERE id='$id'";
 $query=$conn->query($delete);
+echo "
+<script>
+window.location.href = 'courses.php';
+</script>
+";
 if(! $query=$conn->query($delete)){
     echo "kulchi mzn";
 }
