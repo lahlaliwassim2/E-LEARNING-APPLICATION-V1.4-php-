5ce050ec583564dd92bad4295cec4e0b86d812c9
lahlaliwassim2 <wlahlali34@gmail.com>
2022-02-15 09:06:05 +0100

    lier la page par la page etudiant pour la deriger automatiquement

--- deletestud.php
+++ deletestud.php
@@ -4,8 +4,13 @@ if(isset($_GET['id'])){
     $id=$_GET['id'];
 $delete="DELETE FROM students WHERE id='$id'";
 $query=$conn->query($delete);
+echo "
+<script>
+window.location.href = 'espace_etudiantss.php';
+</script>
+";
 if(! $query=$conn->query($delete)){
-    echo "kulchi mzn";
+   
 }
 }
 ?>
