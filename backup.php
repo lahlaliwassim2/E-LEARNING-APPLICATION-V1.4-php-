5be4562392a20e18ca49a8663008ac82694b3b82
lahlaliwassim2 <wlahlali34@gmail.com>
2022-02-19 13:10:20 +0100

    ajouter de code php pour le champ de confirmation de password

--- backup.php
+++ backup.php
@@ -4,6 +4,7 @@
             $name=$_POST['name'];
             $email=$_POST['email'];
             $password=$_POST['password'];
+            $confpassword=$_POST['confpassword'];
             
                 
             if(empty($name)){
@@ -18,13 +19,20 @@
             else if(empty($password)){
                 header('location:sign_up.php?error=entrer votre password');
                 exit();
-            }
-            else{
+            }else{
+                                if($password==$confpassword){
+
+                                
+            
                 $insert="INSERT INTO comptes (name ,email,password) VALUE ('$name','$email','$password')";
                 $result=$conn->query($insert);
                 header('location:indexs.php');    
-            }
+                                                           }
+                                                           else{
+                                                               header('location:sign_up.php?error=password non identique');
+                                                           }
         }
+    }
         else{
             header('location:sign_up.php');
         }
